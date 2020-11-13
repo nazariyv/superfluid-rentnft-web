d1f66094ef2929925674ff7dabc2c6b873d79af4
nazariyv <nazariy@inbox.ru>
2020-11-13 22:00:09 +0000

    ...

--- src/api/ganFace.ts
+++ src/api/ganFace.ts
@@ -1,4 +1,5 @@
 export const getGanFace = async () => {
+  console.log("ley", process.env.GATSBY_RAPID_API_KEY);
   const response = await fetch(
     "https://face-generator.p.rapidapi.com/faces/random",
     {
